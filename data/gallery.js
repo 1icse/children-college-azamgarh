117fc8b1ca8c258f6a1f1a065087c8703b3bba84
Sarthak-Singh <24sarthaksingh2010@gmail.com>
2026-09-04 15:01:50 +0530

    Create gallery data for school images
    Added initial gallery data for Children College Azamgarh.

---
+++ data/gallery.js
@@ -0,0 +1,25 @@
+/*
+ * ==========================================
+ * CHILDREN COLLEGE AZAMGARH
+ * GALLERY DATA
+ * ==========================================
+ *
+ * Add gallery images here.
+ *
+ * Image paths are relative to the HTML files.
+ *
+ * Example:
+ *
+ * image: "../assets/images/gallery/photo1.jpg"
+ *
+ */
+
+window.schoolGallery = [
+
+    {
+        image: "../assets/images/school-background.jpg",
+        alt: "Children College Azamgarh school building",
+        caption: "Children College Azamgarh"
+    }
+
+];
